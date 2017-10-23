2d280922db62e5142a6351275a437866849d20ef
yupeiyong <2758318132@qq.com>
2017-10-23 23:24:47 +0800

    添加数据结构

---
+++ Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack=new Stack<int>();
+            for (var i = 0; i < 50; i++)
+            {
+                stack.Push(i);
+            }
+            while(!stack.IsEmpty())
+                Console.WriteLine(stack.Pop());
+            Console.WriteLine("---------------------------------------------------------------------------\r\n");
+
+            var linkStack=new LinkStack<int>();
+            for (var i = 0; i < 50; i++)
+            {
+                linkStack.Push(i);
+            }
+            while (!linkStack.IsEmpty())
+                Console.WriteLine(linkStack.Pop());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+
+namespace Stack
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack<T>
+    {
+        private T[] _datas;
+
+        public int Count { get; private set; }
+
+        private int _size;
+
+
+        public Stack()
+        {
+            _size = 8;
+            _datas=new T[_size];
+        }
+        public void Push(T data)
+        {
+            if (Count == _size)
+            {
+                _size = _size * 2;
+                var tmp=new T[_size];
+                for (int i = 0, maxCount = _datas.Length; i < maxCount; i++)
+                {
+                    tmp[i] = _datas[i];
+                }
+                _datas = tmp;
+                tmp = null;
+            }
+            _datas[Count++] = data;
+        }
+
+
+        public T Pop()
+        {
+            if(IsEmpty())
+                throw new Exception("栈为空！");
+
+            var index = Count - 1;
+            Count--;
+            return _datas[index];
+        }
+
+
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+        public T GetTop()
+        {
+            if (IsEmpty())
+                throw new Exception("栈为空！");
+
+            var index = Count - 1;
+            return _datas[index];
+        }
+    }
+
+    public class LinkStack<T>
+    {
+
+        private class StackNode<TNode>
+        {
+            public TNode Data { get; set; }
+            public StackNode<TNode> Next { get; set; }
+        }
+
+        public int Count { get; private set; }
+
+        private StackNode<T> _head = null;
+        public LinkStack()
+        {
+            _head = new StackNode<T>
+            {
+                Data = default(T),
+                Next = null
+            };
+        }
+        public void Push(T data)
+        {
+            if (_head == null)
+            {
+                _head = new StackNode<T> {Data = data, Next = null};
+            }
+            else
+            {
+                var node= new StackNode<T> { Data = data, Next = null };
+                var parent = _head;
+                var curNode = _head.Next;
+                while (curNode != null)
+                {
+                    parent = curNode;
+                    curNode = curNode.Next;
+                }
+                curNode = node;
+                parent.Next = curNode;
+            }
+            Count++;
+        }
+
+
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new Exception("栈为空！");
+
+            if (IsEmpty())
+                throw new Exception("栈为空！");
+
+            var parentNode = _head;
+            var curNode = _head.Next;
+            while (curNode.Next != null)
+            {
+                parentNode = curNode;
+                curNode = curNode.Next;
+            }
+            var data = curNode.Data;
+            parentNode.Next = null;
+            Count--;
+            return data;
+        }
+
+
+        public bool IsEmpty()
+        {
+            return Count == 0 || _head.Next==null;
+        }
+        public T GetTop()
+        {
+            if (IsEmpty())
+                throw new Exception("栈为空！");
+
+            var curNode = _head.Next;
+            var parentNode = _head;
+            while (curNode.Next != null)
+            {
+                parentNode = curNode;
+                curNode = curNode.Next;
+            }
+            var data = curNode.Data;
+            return data;
+        }
+    }
+}
