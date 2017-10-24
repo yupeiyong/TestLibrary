ade2e3456dc0a201cf20380921c0c6f93ef0c6e2
yupeiyong <2758318132@qq.com>
2017-10-24 15:03:38 +0800

    add tree

---
+++ Tree/BinaryTree.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Tree
+{
+
+    public class BinaryTree<T>
+    {
+
+        private BinaryTreeNode<T> _head;
+
+
+        public void Add(T data)
+        {
+            var node = new BinaryTreeNode<T> {Data = data};
+            if (_head == null)
+            {
+                _head = node;
+                return;
+            }
+
+            //按层序添加新元素
+            var queue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(_head);
+            while (queue.Count > 0)
+            {
+                var curNode = queue.Dequeue();
+                if (curNode.Left == null)
+                {
+                    curNode.Left = node;
+                    return;
+                }
+                queue.Enqueue(curNode.Left);
+                if (curNode.Rigth == null)
+                {
+                    curNode.Rigth = node;
+                    return;
+                }
+                queue.Enqueue(curNode.Rigth);
+            }
+        }
+
+
+        public bool Find(T data)
+        {
+            if (_head == null)
+            {
+                return false;
+            }
+
+            //按层序添加新元素
+            var queue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(_head);
+            while (queue.Count > 0)
+            {
+                var curNode = queue.Dequeue();
+                if (curNode.Data.Equals(data))
+                    return true;
+                if (curNode.Left != null)
+                {
+                    queue.Enqueue(curNode.Left);
+                }
+
+                if (curNode.Rigth != null)
+                {
+                    queue.Enqueue(curNode.Rigth);
+                }
+            }
+            return false;
+        }
+
+
+        public void Preorder(Action<T> action)
+        {
+            PreorderNode(_head, action);
+        }
+
+
+        private void PreorderNode(BinaryTreeNode<T> root, Action<T> action)
+        {
+            if (root != null)
+            {
+                action(root.Data);
+                PreorderNode(root.Left, action);
+                PreorderNode(root.Rigth, action);
+            }
+        }
+
+
+        public void Inorder(Action<T> action)
+        {
+            InorderNode(_head, action);
+        }
+
+
+        private void InorderNode(BinaryTreeNode<T> root, Action<T> action)
+        {
+            if (root != null)
+            {
+                action(root.Data);
+                InorderNode(root.Left, action);
+                InorderNode(root.Rigth, action);
+            }
+        }
+
+
+        public void Postorder(Action<T> action)
+        {
+            PostorderNode(_head, action);
+        }
+
+
+        private void PostorderNode(BinaryTreeNode<T> root, Action<T> action)
+        {
+            if (root != null)
+            {
+                action(root.Data);
+                PostorderNode(root.Left, action);
+                PostorderNode(root.Rigth, action);
+            }
+        }
+
+
+        /// <summary>
+        ///     层序遍历
+        /// </summary>
+        /// <param name="action"></param>
+        public void Levelorder(Action<T> action)
+        {
+            if (_head == null)
+            {
+                return;
+            }
+
+            //按层序添加新元素
+            var queue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(_head);
+            while (queue.Count > 0)
+            {
+                var curNode = queue.Dequeue();
+                action(curNode.Data);
+                if (curNode.Left != null)
+                {
+                    queue.Enqueue(curNode.Left);
+                }
+
+                if (curNode.Rigth != null)
+                {
+                    queue.Enqueue(curNode.Rigth);
+                }
+            }
+        }
+
+    }
+
+    public class BinaryTreeNode<T>
+    {
+
+        public T Data { get; set; }
+
+        public BinaryTreeNode<T> Left { get; set; }
+
+        public BinaryTreeNode<T> Rigth { get; set; }
+
+    }
+
+}
---
+++ Tree/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree=new BinaryTree<int>();
+
+            for (var i = 0; i < 20; i++)
+            {
+                tree.Add(i);
+            }
+
+            Console.WriteLine("\r\n先序遍历");
+            tree.Preorder(d=>Console.Write(d+", "));
+            Console.WriteLine("\r\n中序遍历");
+            tree.Inorder(d => Console.Write(d + ", "));
+            Console.WriteLine("\r\n后序遍历");
+            tree.Postorder(d => Console.Write(d + ", "));
+            Console.WriteLine("\r\n层序遍历");
+            tree.Levelorder(d => Console.Write(d + ", "));
+
+            Console.WriteLine("\r\n查找15");
+            var result=tree.Find(15);
+            if (result)
+            {
+                Console.WriteLine("\r\n查找15成功！");
+            }
+            else
+            {
+                Console.WriteLine("\r\n查找15失败！");
+            }
+            Console.ReadKey();
+        }
+    }
+}
