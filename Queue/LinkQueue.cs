6fed01220da667fa52532b7c5515f4d28caf3985
yupeiyong <2758318132@qq.com>
2017-10-24 12:23:33 +0800

    add queue

---
+++ Queue/LinkQueue.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+
+namespace Queue
+{
+
+    /// <summary>
+    ///     链式队列
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LinkQueue<T>
+    {
+
+        /// <summary>
+        ///     尾部
+        /// </summary>
+        private Node<T> _tail;
+
+        /// <summary>
+        ///     头部
+        /// </summary>
+        private Node<T> _top;
+
+        public int Count { get; private set; }
+
+
+        /// <summary>
+        ///     从 Queue 中移除所有的元素。
+        /// </summary>
+        public void Clear()
+        {
+            while (!IsEmpty())
+            {
+                Dequeue();
+            }
+        }
+
+
+        /// <summary>
+        ///     判断某个元素是否在 Queue 中。
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Contains(T data)
+        {
+            if (IsEmpty())
+                throw new Exception("队列为空！");
+
+            var curNode = _top;
+            while (curNode.Next != null)
+            {
+                if (curNode.Data.Equals(data))
+                {
+                    return true;
+                }
+                curNode = curNode.Next;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        ///     移除并返回在 Queue 的开头的对象。
+        /// </summary>
+        /// <returns></returns>
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new Exception("队列为空！");
+
+            var data = _top.Data;
+            _top = _top.Next;
+            Count--;
+            return data;
+        }
+
+
+        public bool IsEmpty()
+        {
+            return Count == 0 || _top == null;
+        }
+
+
+        /// <summary>
+        ///     向 Queue 的末尾添加一个对象。
+        /// </summary>
+        /// <param name="data"></param>
+        public void Enqueue(T data)
+        {
+            var node = new Node<T> {Data = data};
+            Count++;
+            if (_tail == null)
+            {
+                _tail = node;
+                _top = node;
+                return;
+            }
+            _tail.Next = node;
+            _tail = _tail.Next;
+        }
+
+
+        private class Node<TNode>
+        {
+
+            public TNode Data { get; set; }
+
+            public Node<TNode> Next { get; set; }
+
+        }
+
+    }
+
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue=new LinkQueue<int>();
+            for (var i = 0; i < 50; i++)
+            {
+                queue.Enqueue(i);
+            }
+            Console.WriteLine("\r\n查找45---------------------------------------------------------------");
+            var result = queue.Contains(45);
+            if (result == false)
+            {
+                Console.WriteLine("未找到元素");
+            }
+            else
+            {
+                Console.WriteLine("找到元素：45");
+            }
+
+            while (!queue.IsEmpty())
+            {
+                Console.Write(queue.Dequeue()+", ");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
