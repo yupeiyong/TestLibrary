cc4b90d5c62e291f4ef75913f3a77a4a8feb19d2
yupeiyong <2758318132@qq.com>
2017-10-27 09:52:17 +0800

    Add CSharp helper files

---
+++ TestProject/CompositeNumber/UnitTestCompositeNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject.CompositeNumber
+{
+    [TestClass]
+    public class UnitTestCompositeNumber
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            var compositeNumber=new CompositeNumber(1,23);
+            var numbers = compositeNumber.CompositeNumbers;
+        }
+    }
+}
