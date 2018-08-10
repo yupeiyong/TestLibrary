f04200fa053a1b8e413370b2c12e0ab68d9b2c05
yupeiyong <peiyongyu@163.com>
2018-08-10 11:52:34 +0800

    拉取前提交

---
+++ TestProject/Threads/UnitTestAwait.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject.Threads
+{
+    [TestClass]
+    public class UnitTestAwait
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result=Test();
+            Task.WaitAll();
+        }
+
+
+        public async Task<int> Test()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1000*200);
+                return 0;
+            });
+        }
+    }
+}
